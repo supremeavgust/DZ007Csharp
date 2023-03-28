bac70764002674cc7f4f6c33167d5d16754543ff
Stanislav <avgustwot@mail.ru>
2023-03-28 20:20:07 +0300

    Задача 50

---
+++ example50/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции 
+// элемента в двумерном массиве, и возвращает значение этого элемента
+// или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+void Print(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"[{matrix[i, j]}]   ");
+        Console.WriteLine();
+    }
+
+}
+void ArrayRandom(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11);
+    }
+}
+// void FindChar(int[,] matrix)
+// {
+//     Console.WriteLine("Введите координаты");
+//     int a = Convert.ToInt32(Console.ReadLine());
+//     int b = Convert.ToInt32(Console.ReadLine());
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//             if (a > i && b > j)
+//         Console.WriteLine("такого числа нет");
+//     else
+//     {
+//         object c = matrix.GetValue(a, b);
+//         Console.WriteLine(c);
+//     }
+//     }
+
+// }
+Console.Clear();
+int i = new Random().Next(2, 5);
+int j = new Random().Next(2, 5);
+int[,] matrix = new int[i, j];
+
+ArrayRandom(matrix);
+Print(matrix);
+Console.Write("Введите позицию строки: ");
+int k = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позицию столбца: ");
+int b = Convert.ToInt32(Console.ReadLine());
+    if (k < 1 || b < 1)
+    Console.Write("Позиции строк не могут быть отрицательными");
+    else if (k == i + 1 && b == j + 1)
+    Console.Write($"Значение элемента равно {matrix[k - 1, b - 1]}");
+    else Console.Write("Такого элемента нет в массиве");
---
+++ example52/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 100);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+void ReleaseMatrix(int[,] matrix)
+{
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double summa = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+
+            summa += matrix[i, j];
+
+        }
+                  Console.Write($"{summa/matrix.GetLength(0):F2} \t");
+    }
+}
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
